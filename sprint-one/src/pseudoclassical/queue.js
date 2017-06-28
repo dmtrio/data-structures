3d7cda45c1d65ff17e11ec79446b3e798bebca2c
Hack Reactor Student <student@hackreactor.com>
2017-06-28 12:23:00 -0500

    Completed pseudoclassical for stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
+  this.storage = {};
+  this.sized = 0; 
+  this.smallestValue = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.sized] = value;
+  this.sized++;
+    
+};
+Queue.prototype.dequeue = function() {
+  var tempStorage = this.storage[this.smallestValue];
+  delete this.storage[this.smallestValue];
+  if (this.sized > this.smallestValue) {
+    this.smallestValue++;
+  }
+  return tempStorage;
 
-
+};
+Queue.prototype.size = function() {
+  return this.sized - this.smallestValue;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
+  this.storage = {};
+  this.sized = 0; 
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.sized] = value;
+  this.sized++;
+    
+};
+Stack.prototype.pop = function() {
+  if (this.sized > 0) {
+    this.sized--;
+  }
+  return this.storage[this.sized];
 
+};
+Stack.prototype.size = function() {
+  return this.sized;
+};
