df538b40076466bebc558b80c4bf063b592bc704
Hack Reactor Student <student@hackreactor.com>
2017-06-28 11:55:12 -0500

    Completed functional-shared for stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
+  var newQueue = {};
+  newQueue.storage = {};
+  newQueue.sized = 0;
+  newQueue.smallestValue = 0;
+  _.extend(newQueue, queueMethods);
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return newQueue;
 };
 
 var queueMethods = {};
-
+queueMethods.enqueue = function(value) {
+  this.storage[this.sized] = value;
+  this.sized++;
+};
+queueMethods.dequeue = function() {
+  var tempStorage = this.storage[this.smallestValue];
+  delete this.storage[this.smallestValue];
+  if (this.sized > this.smallestValue) {
+    this.smallestValue++;
+  }
+  return tempStorage;
+};
+queueMethods.size = function() {
+  return this.sized - this.smallestValue;
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
+  var newStack = {};
+  newStack.storage = {};
+  newStack.sized = 0;
+  _.extend(newStack, stackMethods);
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return newStack;
 };
 
 var stackMethods = {};
-
-
+stackMethods.push = function(value) {
+  this.storage[this.sized] = value;
+  this.sized++;
+};
+stackMethods.pop = function() {
+  if (this.sized > 0) {
+    this.sized--;
+  }
+  return this.storage[this.sized];
+};
+stackMethods.size = function() {
+  return this.sized;
+};
