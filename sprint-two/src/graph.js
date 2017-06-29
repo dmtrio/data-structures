3ceaceb548367df88922daa2130b34c803279851
Hack Reactor Student <student@hackreactor.com>
2017-06-29 11:08:18 -0500

    Completed Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -28,57 +28,45 @@ Graph.prototype.removeNode = function(node) {
       delete this[index];
     } 
   }
+  if (this.hasOwnProperty('edges')) {
+    this.removeEdge(node, null);
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  var fromEdge = false;
-  var toEdge = false;
-  for (var index = 0; index < Object.keys(this).length; index++) {
-    if (this[index].edgeWith === fromNode) {
-      toEdge = true;
-    } 
-    if (this[index].edgeWith === toNode) {
-      fromEdge = true;
+  for (var edg = this.edges.length - 1; edg > -1; edg--) {
+    if ((this.edges[edg][0] === fromNode && this.edges[edg][1] === toNode) || (this.edges[edg][0] === toNode && this.edges[edg][1] === fromNode)) {
+      return true;
     }
   }
-  if (fromEdge && toEdge) {
-    return true;
-  } else {
-    return false;
-  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  for (var index = 0; index < Object.keys(this).length; index++) {
-    if (this[index].value === fromNode) {
-      this[index].edgeWith = toNode;
-    } else if (this[index].value === toNode) {
-      this[index].edgeWith = fromNode;
-    }
+  if (!this.hasOwnProperty('edges')) {
+    this.edges = [];
   }
+  this.edges.push([fromNode, toNode]);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  for (var index = 0; index < Object.keys(this).length; index++) {
-    if (this[index].edgeWith === fromNode) {
-      delete this[index].edgeWith;
-    } else if (this[index].edgeWith === toNode) {
-      delete this[index].edgeWith;
+  for (var edg = this.edges.length - 1; edg > -1; edg--) {
+    if ((this.edges[edg][0] === fromNode && this.edges[edg][1] === toNode) || (this.edges[edg][0] === fromNode && this.edges[edg][1] === fromNode) || (this.edges[edg][0] === fromNode && toNode === null) || (this.edges[edg][1] === fromNode && toNode === null)) {
+      return this.edges.splice(this.edges[edg], 1);
     }
-  }  
-
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
   for (var key in this) {
     if (!isNaN(key)) {
-      console.log(this[key].value);
-      console.log(cb.apply(this[key].value, arguments));
-    }
+      cb(this[key].value);
+      console.log(JSON.stringify(cb(this[key].value)), this[key].value);
+    } 
   }
   
   
